47dbfd68580e6a19156d64f59ac6ecfd4bc31778
Mariamekh <60445999+Mariamekh@users.noreply.github.com>
2022-02-03 23:44:43 +0400

    feat: implement client side with apollo

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { useQuery, useMutation } from "@apollo/client";
+import { GET_ALL_USERS } from "./query/user";
+import { CREATE_USER } from "./mutation/user";
 
 function App() {
+  const { data, loading, error, refetch } = useQuery(GET_ALL_USERS);
+  const [newUser] = useMutation(CREATE_USER);
+  const [users, setUsers] = useState([]);
+  const [username, setUsername] = useState("");
+  const [age, setAge] = useState(0);
+  console.log(data, "data");
+  useEffect(() => {
+    if (!loading) {
+      setUsers(data.getAllUsers);
+    }
+    if (loading) {
+      return <h1>Failed loading data</h1>;
+    }
+  }, [data]);
+
+  const addUser = (e) => {
+    e.preventDefault();
+    newUser({
+      variables: {
+        input: {
+          username,
+          age,
+        },
+      },
+    }).then(({ data }) => {
+      console.log(data);
+      setUsername("");
+      setAge(0);
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <form>
+        <input
+          type='text'
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type='number'
+          value={age}
+          onChange={(e) => setAge(e.target.value)}
+        />
+        <div>
+          <button onClick={(e) => addUser(e)}>Create</button>
+          <button>Get</button>
+        </div>
+      </form>
+      <div>
+        {users.map((user) => (
+          <div key={user.id} className='user'>
+            {user.id}. {user.username} {user.age}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
